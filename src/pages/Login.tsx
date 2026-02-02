1d590c4459a0797f04e08490fa4a85bbf0314983
OnruBekrant <cerimgt@gmail.com>
2026-02-02 17:16:40 +0300

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import '../styles/Login.css'; // CSS import edildi
+
 
 function Login() {
     const [passIsVisible, setpassIsVisible] = useState(false);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"], // 1. Bunu ekliyoruz (shadcn renk teması için şart)
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,5 +8,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate") // 2. EN KRİTİK KISIM: Animasyon paketi buraya eklenmeli
+  ],
 }
